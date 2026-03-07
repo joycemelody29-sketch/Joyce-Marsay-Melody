d48dd6f98455c5c99b07296241499543ca6dade6
joycemelody29-sketch <joycemelody29@gmail.com>
2026-03-07 09:19:54 +0700

    Update program_stm32_internal_platformio.ino

--- Praktikum Sistem Embedded/Modul 2 Interrupt Timer/program_stm32_internal_platformio.ino
+++ Praktikum Sistem Embedded/Modul 2 Interrupt Timer/program_stm32_internal_platformio.ino
@@ -1,1 +1,22 @@
+; PlatformIO - TIM2 Periodic Interrupt
 
+[env:bluepill_f103c8]
+platform = ststm32
+board = bluepill_f103c8
+framework = arduino
+upload_protocol = stlink
+monitor_speed = 115200
+
+; For ST-Link V2
+debug_tool = stlink
+
+upload_flags =
+    -c
+    set CPUTAPID 0x2ba01477
+
+build_flags =
+    -D LED_BUILTIN=PA0
+
+lib_deps =
+    adafruit/Adafruit SSD1306@^2.5.7
+    adafruit/Adafruit GFX Library@^1.11.3
